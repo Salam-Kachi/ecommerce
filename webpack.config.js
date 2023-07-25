0a80faecbbb11b7f48bd7c19e105dccd5e8eb085
Salam <markeitng1972@yahoo.com>
2023-07-25 23:35:47 +0300

    search page done

--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,10 @@ module.exports = {
     filename:"payment.html",
     template:"./src/payment.html"
 }),
+  new HtmlWebpackPlugin({
+    filename:"search.html",
+    template:"./src/search.html"
+}),
     new HtmlWebpackPlugin({
       filename:"checkout.html",
       template:"./src/checkout.html"
